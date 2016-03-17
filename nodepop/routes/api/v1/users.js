91dbc58d2ec442ffd9ecf4aded4c6d0ca84214fc
Miriam Merchante <miriam.merchante@babel.es>
2016-03-17 09:36:58 +0100

    Get, post, put, delete de usuarios

--- nodepop/routes/api/v1/users.js
+++ nodepop/routes/api/v1/users.js
@@ -2,14 +2,16 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
+var crypto = require("crypto");
 var User = mongoose.model('User');
+
 
 //Si queremos llevar la autenticación a otros módulos, sólo hace falta copiar las dos líneas siguientes
 var auth = require('../../../lib/auth');
-router.use(auth('admin', 'pass2'));
+router.use(auth());
 
 
-/* GET users listing. */
+/*----------------------------Get-----------------------------------------*/
 router.get('/', function(req, res) {
     var sort = req.query.sort || 'name';
     User.list(sort, function(err, rows) {
@@ -25,22 +27,62 @@ router.get('/', function(req, res) {
 
 });
 
-// Añadir un user
+/*----------------------------Post-----------------------------------------*/
 
-router.post('/', function(req, res) {
-    // Instanciamos objeto en memoria
-    var user = new User(req.body);
-    // Lo guardamos en la BD
-    user.save(function(err, newRow) {
+router.post('/', function(req, res, next) {
+    var nombre = req.body.nombre;
+    var email = req.body.email;
+    var password = req.body.clave;
+    let filter = {};
+    filter.nombre = nombre;
+    console.log(filter.nombre);
+
+    User.findOne(filter, function(err, row) {
         if (err) {
-            res.json({ result: false, err: err });
+            res.send('error', err);
             return;
         }
-        res.json({ result: true, row: newRow });
+        if (!row) {
+            console.log("row",row);
+            var sha256 = crypto.createHash("sha256");
+            sha256.update(password, "utf8"); //utf8 here
+            var result = sha256.digest("base64");
+
+
+            var user = new User({ nombre: nombre, email: email, clave: result });
+            user.save(function(err, user) {
+                if (err) {
+                    console.log("Error!" + err);
+                    return;
+                }
+                res.send('User creado.- \n' + "Nombre: " + user.nombre + "\n" + "Email: " + user.email);
+                return;
+
+                console.log('User creado: ' + user);
+
+            });
+        } else res.send("Usuario ya registrado");
+
     });
 });
 
-//Actualizar un user
+
+
+
+/*----------------------------Delete-----------------------------------------*/
+
+
+router.delete('/:id', function(req, res) {
+    User.remove({ _id: req.params.id }, function(err) {
+        if (err) {
+            res.json({ err });
+            return;
+        }
+        res.json("Usuario eliminado");
+    });
+});
+
+/*----------------------------Update-----------------------------------------*/
 
 router.put('/:id', function(req, res) {
     //Para actualizar varios hay que usar en options
@@ -54,16 +96,6 @@ router.put('/:id', function(req, res) {
 
         res.json({ result: true, row: data });
 
-    });
-});
-
-router.delete('/:id', function(req, res) {
-    User.remove({ _id: req.params.id }, function(err) {
-        if (err) {
-            res.json({err});
-            return;
-        }
-        res.json("Usuario eliminado");
     });
 });
 
